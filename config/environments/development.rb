c6ad45a1446a84671ecc73217f75b1010412ce17
Ricardo Uemura <uemuradev.exe@gmail.com>
2025-08-12 14:47:47 +0900

    added bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # Configure public file server for serving static files
+  config.public_file_server.enabled = true
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
